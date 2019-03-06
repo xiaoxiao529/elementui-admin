77f1bf8224a11f87e7eb383ae880d91a3ce91eb5
xiaoxiao529 <271344507.com>
2019-03-06 11:17:26 +0800

    190306 commit

--- src/mock/mock.js
+++ src/mock/mock.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 import Mock from 'mockjs';
 import { Users } from './data/user.js'; // 导入Users数据
-
+let _Users = Users;
 export default {
   /**
    * mock start
@@ -10,21 +10,92 @@ export default {
   start() { // 初始化函数
     let mock = new MockAdapter(axios); // 创建 MockAdapter 实例
 
-        //获取用户列表
-    mock.onGet('/user/list').reply(config => { //  config 指 前台传过来的值  网址自己随意定义，访问时要和这个网址一致就可以，这个'/user/list'，就是get请求时的url地址
+    //获取用户列表接口
+    mock.onGet('/user/list',).reply(config => { //  config 指 前台传过来的值  网址自己随意定义，访问时要和这个网址一致就可以，这个'/user/list'，就是get请求时的url地址
       /*let {name} = config.params;
       let mockUsers = Users.filter(user => {
         if (name && user.name.indexOf(name) == -1) return false;
         return true;
       });*/
+     //console.log(config)
       return new Promise((resolve, reject) => {  //响应请求，返回数据给前台
         setTimeout(() => {
           resolve([200, {
-            users: Users
+            users: _Users
           }]);
         }, 1000);
       });
     });
+    
+    //增加用户接口
+    mock.onGet('/user/addUser').reply(config=>{
+    	let {id,username,age,sex,date1,address} = config.params;
+    	_Users.push({
+    		id:id,
+    		name:username,
+    		address:address,
+    		age:age,
+    		birth:date1,
+    		sex:sex
+    	})
+    	return new Promise((resolve, reject) => {  //响应请求，返回数据给前台
+        setTimeout(() => {
+          resolve([200, {
+            code:200,
+            msg:'新增成功'
+          }]);
+        }, 1000);
+      });
+    	
+    })
+    
+    //删除用户接口
+		mock.onGet('/user/deleteUser').reply(config=>{
+			let _id = config.params.id;
+			
+			_Users = _Users.filter((value,key,arr)=>{
+				return value.id !== _id
+			})
 
+			return new Promise((resolve, reject) => {  //响应请求，返回数据给前台
+        setTimeout(() => {
+          resolve([200, {
+            code:200,
+            msg:'删除成功'
+          }]);
+        }, 1000);
+      });
+			
+			
+		})
+		
+		//编辑用户接口
+		mock.onGet('/user/editUser').reply(config=>{
+			let {id,username,age,sex,date1,address} = config.params;
+			_Users.some((value,key,arr)=>{
+				if(value.id === id){
+					value.id = id;
+					value.username = username;
+					value.age = age;
+					value.sex = sex;
+					value.date1 = date1;
+					value.address = address;
+				}
+			})
+
+			return new Promise((resolve, reject) => {  //响应请求，返回数据给前台
+        setTimeout(() => {
+          resolve([200, {
+            code:200,
+            msg:'编辑成功'
+          }]);
+        }, 1000);
+      });
+			
+			
+		})
+		
+		
+		
   }
 };
--- src/views/Table.vue
+++ src/views/Table.vue
@@ -1,12 +1,12 @@
 <template>
   
-  <div id="table">
+  <div id="table" v-loading="loading">
   	<h3>Table</h3>
   	<el-row>
   		
 	  <el-col :span="4">
 	  	<div class="grid-content bg-purple">
-		  	<el-input  placeholder="请输入内容"></el-input>
+		  	<el-input  placeholder="请输入内容" v-model="username"></el-input>
 		</div>
 	  </el-col>
 	  <el-col :span="1">
@@ -14,8 +14,8 @@
 	  </el-col>
 	  <el-col :span="12">
 	  	<div class="grid-content bg-purple-light">
-	  	  <el-button type="primary">查询</el-button>
-	  	  <el-button type="primary">新增</el-button>
+	  	  <el-button type="primary"  @click="searchUser()">查询</el-button>
+	  	  <el-button type="primary" @click="addUser()">新增</el-button>
 	  	</div>
 	  </el-col>
 	  
@@ -27,9 +27,7 @@
 	    style="width: 100%"
 	    @selection-change="handleSelectionChange"
 	    >
-	    
-	    
-	    
+	
 	    <el-table-column
 	      type="selection"
 	      width="55">
@@ -103,6 +101,91 @@
 		  @current-change="current_change"
 		  >
 		</el-pagination>
+		
+		<!--新增界面框-->
+		<el-dialog title="新增" :visible.sync="dialogFormVisible">
+			
+		  <el-form :model="form" :rules="addRules" ref="addRuleForm" label-width="80px">
+		  	
+		    <el-form-item label="姓名" prop="name">
+		      <el-input v-model="form.name" autocomplete="off"></el-input>
+		    </el-form-item>
+		    
+		    <el-form-item label="性别">
+			    <el-radio-group v-model="form.sex">
+			      <el-radio :label="1">男</el-radio>
+			      <el-radio :label="0">女</el-radio>
+			    </el-radio-group>
+			  </el-form-item>
+		    
+		    <el-form-item label="年龄" prop="age">
+			    <el-input v-model.number="form.age"></el-input>
+			  </el-form-item>
+			  
+			  <el-form-item label="生日" required>
+			    <el-col :span="24">
+			      <el-form-item prop="date1">
+			        <el-date-picker  type="date" format="yyyy-MM-dd" value-format="yyyy-MM-dd" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
+			      </el-form-item>
+			    </el-col>
+			  </el-form-item>
+		    
+		    <el-form-item label="地址" prop="address">
+			    <el-input type="textarea" v-model="form.address"></el-input>
+			  </el-form-item>
+			    
+		  </el-form>
+		  
+		  <div slot="footer" class="dialog-footer">
+		    <el-button @click="dialogFormVisible = false">取 消</el-button>
+		    <el-button type="primary" @click="addUserData()">确 定</el-button>
+		  </div>
+		</el-dialog>
+		
+		
+		<!--编辑界面框-->
+		<el-dialog title="编辑" :visible.sync="editFormVisible">
+			
+		  <el-form :model="editform" :rules="editRules" ref="editRuleForm" label-width="80px">
+		  	
+		    <el-form-item label="姓名" prop="name">
+		      <el-input v-model="editform.name" autocomplete="off"></el-input>
+		    </el-form-item>
+		    
+		    <el-form-item label="性别">
+			    <el-radio-group v-model="editform.sex">
+			      <el-radio :label="1">男</el-radio>
+			      <el-radio :label="0">女</el-radio>
+			    </el-radio-group>
+			  </el-form-item>
+		    
+		    <el-form-item label="年龄" prop="age">
+			    <el-input v-model.number="editform.age"></el-input>
+			  </el-form-item>
+			  
+			  <el-form-item label="生日" required>
+			    <el-col :span="24">
+			      <el-form-item prop="date1">
+			        <el-date-picker  type="date" format="yyyy-MM-dd" value-format="yyyy-MM-dd" placeholder="选择日期" v-model="editform.date1" style="width: 100%;"></el-date-picker>
+			      </el-form-item>
+			    </el-col>
+			  </el-form-item>
+		    
+		    <el-form-item label="地址" prop="address">
+			    <el-input type="textarea" v-model="editform.address"></el-input>
+			  </el-form-item>
+			    
+		  </el-form>
+		  
+		  <div slot="footer" class="dialog-footer">
+		    <el-button @click="editFormVisible = false">取 消</el-button>
+		    <el-button type="primary" @click="editSubmit()">确 定</el-button>
+		  </div>
+		</el-dialog>
+		
+		
+		
+		
   </div>
   
 </template>
@@ -119,7 +202,38 @@
         },
         totalPage:0,
         pageSize:10,
-        currentPage:1
+        currentPage:1,
+        loading: true,
+        username:'',  //查询的用户名称
+        dialogFormVisible: false,//新增框
+        editFormVisible:false,//编辑框
+        form: {
+          name: '',
+          sex:-1,
+          age: '',
+          date1: '',
+          address:''
+        },
+        editform: {
+          name: '',
+          sex:-1,
+          age: '',
+          date1: '',
+          address:''
+        },
+        formLabelWidth: '120px',
+        addRules:{
+        	name: [
+            { required: true, message: '请输入姓名', trigger: 'blur' },
+            { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+          ]
+        },
+        editRules:{
+        	name: [
+            { required: true, message: '请输入姓名', trigger: 'blur' },
+            { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+          ]
+        }
       }
     },
 
@@ -136,11 +250,96 @@
       handleSelectionChange(val) {
         this.multipleSelection = val;
       },
+      
+      //编辑当前用户数据
       handleEdit(index, row) {
-        console.log(index, row);
+      	
+      	this.editFormVisible = true;
+        this.editform = Object.assign({},row)
+        
+        console.log(row)
+
       },
+      
+      editSubmit(){
+      	this.$confirm('是否编辑本条数据?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+        		this.dialogFormVisible = true;
+	        	let username = this.form.name;
+		      	let age = this.form.age;
+		      	let sex = this.form.sex;
+		      	let date1 = this.form.date1;
+		      	let address = this.form.address;
+						let param = {
+		        	id:row.id,
+		        	username,
+		      		age,
+		      		sex,
+		      		date1,
+		      		address
+		        }
+	        	axios.get('/user/editUser',{params:param}).then((response)=>{
+	        		if(response.data.code === 200){
+	        			this.$message({
+			            type: 'success',
+			            message: '编辑成功!'
+			          });
+			          this.getUser();
+			          this.dialogFormVisible = false;
+	        		}
+	        	
+	        	
+		        }).catch((error)=>{
+		        	console.log(error)
+		        })
+        	
+        	
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消编辑'
+          });          
+        });
+      },
+      
+      //删除当前用户数据
       handleDelete(index, row) {
-        console.log(index, row);
+        
+        
+        this.$confirm('是否删除本条数据?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+						let param = {
+		        	id:row.id
+		        }
+	        	axios.get('/user/deleteUser',{params:param}).then((response)=>{
+	        		if(response.data.code === 200){
+	        			this.$message({
+			            type: 'success',
+			            message: '删除成功!'
+			          });
+			          this.getUser()
+	        		}
+	        	
+	        	
+		        }).catch((error)=>{
+		        	console.log(error)
+		        })
+        	
+        	
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          });          
+        });
+        
+        
       },
       //性别转换
       formatSex(row, column) {
@@ -148,16 +347,104 @@
       },
       current_change(currentPage){
       	this.currentPage = currentPage;
+      },
+      searchUser(){
+
+				if(this.username == '' ){
+					this.getUser()
+					return ;
+				}
+      	this.tableData3 =  this.tableData3.filter((value,key,arr)=>{
+      		return value.name === this.username
+      	})
+      	
+      },
+      
+      //封装请求用户信息方法
+      getUser(){
+      	this.loading = true;
+//    	var searchObj = {
+//    		username:this.username
+//    	}
+      	axios.get('/user/list'/*,{params:searchObj}*/).then((response)=>{
+	    		this.tableData3 = response.data.users
+	    		this.totalPage = response.data.users.length
+	    		this.loading = false;
+					console.log(this.tableData3)
+	    	}).catch((error)=>{
+	    		console.log(error)
+	    	})
+      },
+      //新增用户
+      addUser(){
+      	this.dialogFormVisible = true
+      },
+      
+      //增加数据
+      addUserData(){
+      	let username = this.form.name;
+      	let age = this.form.age;
+      	let sex = this.form.sex;
+      	let date1 = this.form.date1;
+      	let address = this.form.address;
+      	let addObj = {
+      		id:new Date().getTime(),
+      		username,
+      		age,
+      		sex,
+      		date1,
+      		address
+      	}
+
+      	this.$refs['addRuleForm'].validate((valid) => {
+          if (valid) {  //用户填写的数据，校验通过，进行数据提交
+          	
+          	this.$confirm('是否提交?', '提示', {
+			          confirmButtonText: '确定',
+			          cancelButtonText: '取消',
+			          type: 'warning'
+			        }).then(() => {
+			        	this.dialogFormVisible = false;
+			        	
+			        	let _addObj = Object.assign({},addObj);
+			        	
+			        	axios.get('/user/addUser',{params:_addObj}).then((response)=>{
+            			console.log(response)
+				        	if(response.data.code===200){
+					    			this.$message({
+					            type: 'success',
+					            message: '新增成功!'
+					          });
+					          
+					          this.getUser();
+					          this.$refs['addRuleForm'].resetFields();
+					    		}
+	
+					    	}).catch((error)=>{
+					    		console.log(error)
+					    	})
+			        	
+			        	
+			        }).catch(() => {
+			          this.$message({
+			            type: 'info',
+			            message: '已取消新增'
+			          });          
+			        });
+
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      	
+      	
       }
     },
     created(){
     	//请求用户信息
-    	axios.get('/user/list').then((response)=>{
-    		console.log(response)
-    		
-    		this.tableData3 = response.data.users
-    		this.totalPage = response.data.users.length
-    	})
+    	this.getUser();
+    	
     }
   }
 </script>
